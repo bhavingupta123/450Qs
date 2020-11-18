d9bb28613a1ca20e926ae98695d2fcb2c8ec9fb8
Bhavin Gupta <bhavin-cs031@dsce.edu.in>
2020-11-18 23:57:37 +0530

    4th commit

--- Q8_MAX_SUBARRAY.cpp
+++ Q8_MAX_SUBARRAY.cpp
@@ -1,4 +1,6 @@
-/*Q.8 find max sub array sum --- KADANE'S ALGORITHM*/
+/*Q.8 find max sub array sum --- O(N) solution*/
+
+/*
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -30,3 +32,36 @@ int main()
 
     return 0;
 }
+*/
+
+
+/*KADANE'S ALGORITHM approach*/
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+    long long int ele,item,d,n,flag=0,i,s=-1,ans1=0,f=-1,j,k,m,a,b,x,y,cnt1=0,ans3=0,ans=0,max1=0,cnt=0,temp;
+    cin>>n;
+    long long int arr[n];
+
+    for(i=0;i<n;++i)
+        cin>>arr[i];
+
+    long long int end_sum=arr[0],max_sum=arr[0];
+
+    for(i=1;i<n;++i)
+    {
+        end_sum=max(arr[i],arr[i]+end_sum);
+        max_sum=max(max_sum,end_sum);
+    }
+
+    cout<<max_sum;
+
+    return 0;
+}
+
