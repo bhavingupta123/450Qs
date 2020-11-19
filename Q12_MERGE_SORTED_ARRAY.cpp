c4f15fac3060cf40e9e7c68f35759eb61e195249
Bhavin Gupta <bhavin-cs031@dsce.edu.in>
2020-11-19 13:54:10 +0530

    array question 12 accepted soln

--- Q12_MERGE_SORTED_ARRAY.cpp
+++ Q12_MERGE_SORTED_ARRAY.cpp
@@ -1,6 +1,8 @@
-/*Q.12 MERGE TWO SORTED ARRAYS NO EXTRA SPACE*/
-/* INSERTION SORT METHOD USED */
+/*Q.12 MERGE TWO SORTED ARRAYS WITHOUT USING EXTRA SPACE*/
 
+/* METHOD 1 USING INSERTION SORT TECHNIQUE ---> O(N*M)*/
+
+/*
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -45,6 +47,49 @@ int main()
         cout<<arr[i]<<" ";
 
         cout<<"\n";
+
+    for(i=0;i<m;++i)
+        cout<<arr2[i]<<" ";
+
+    return 0;
+}
+*/
+
+/*METHOD 2 ---> O(NlogN)*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long long int ele,item,d,n,flag=0,i,s=-1,ans1=0,f=-1,j,k,m,a,b,x,y,cnt1=0,ans3=0,ans=0,max1=0,cnt=0,temp;
+    cin>>n>>m;
+
+    int arr[n],arr2[m];
+
+    for(i=0;i<n;++i)
+        cin>>arr[i];
+
+    for(i=0;i<m;++i)
+        cin>>arr2[i];
+
+    i=n-1;
+    j=0;
+
+    for(;i>=0,j<m;++j,--i)
+    {
+        if(arr[i]>arr2[j])
+            swap(arr[i],arr2[j]);
+        else
+            break;
+    }
+
+    sort(arr,arr+n);
+    sort(arr2,arr2+m);
+
+    for(i=0;i<n;++i)
+        cout<<arr[i]<<" ";
 
     for(i=0;i<m;++i)
         cout<<arr2[i]<<" ";
