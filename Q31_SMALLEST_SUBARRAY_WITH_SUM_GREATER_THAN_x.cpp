f15fe3d42e9e2d947735abf120c1254e68291dca
Bhavin Gupta <bhavin-cs031@dsce.edu.in>
2020-12-15 11:36:48 +0530

    Q31 updated

--- Q31_SMALLEST_SUBARRAY_WITH_SUM_GREATER_THAN_x.cpp
+++ Q31_SMALLEST_SUBARRAY_WITH_SUM_GREATER_THAN_x.cpp
@@ -7,34 +7,39 @@ int main()
 {
     long long t,start,end1,sum,index,ele,rem,item,d,n,n1,n2,n3,flag=0,i,s=-1,ans1=0,f=-1,j,k,m,a,b,x,y,cnt1=0,ans3=0,ans=0,max1=0,cnt=0;
 
-    cin>>n>>m;
-
-    long long int arr[n];
-
-    for(i=0;i<n;++i)
+    cin>>t;
+    while(t--)
     {
-        cin>>arr[i];
-    }
+       cin>>n>>m;
 
-    start=end1=0;
+        long long int arr[n];
 
-    long long int curr_sum=0,min_len=n+1;
-
-    while(end1<n)
-    {
-        while(curr_sum<=m && end1<n)
-            curr_sum+=arr[end1++];
-
-        while(curr_sum>m && start<n)
+        for(i=0;i<n;++i)
         {
-            if(end1-start<=min_len)
-                min_len=end1-start;
-
-            curr_sum-=arr[start++];
+            cin>>arr[i];
         }
-    }
 
-    cout<<min_len;
+        start=end1=0;
+
+        long long int curr_sum=0,min_len=n+1;
+
+        while(end1<n)
+        {
+            while(curr_sum<=m && end1<n)
+                curr_sum+=arr[end1++];
+
+            while(curr_sum>m && start<n)
+            {
+                if(end1-start<=min_len)
+                    min_len=end1-start;
+
+                curr_sum-=arr[start++];
+            }
+        }
+
+        cout<<min_len<<"\n";
+
+    }
 
     return 0;
 }
