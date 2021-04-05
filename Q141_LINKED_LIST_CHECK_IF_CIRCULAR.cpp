088982ccaa537e9e64d70b95815342287c2aa785
Bhavin Gupta <bhavin-cs031@dsce.edu.in>
2021-04-05 12:42:51 +0530

    linked list continue

---
+++ Q141_LINKED_LIST_CHECK_IF_CIRCULAR.cpp
@@ -0,0 +1,12 @@
+bool isCircular(Node *head)
+{
+    if(head==NULL)
+        return true;
+    
+    Node *temp = head->next;
+    
+    while(temp!=NULL && temp!=head)
+        temp = temp->next;
+        
+    return (temp == head);
+}
